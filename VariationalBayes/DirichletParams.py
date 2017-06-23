dc5c899468a30c12e3b969dfa919dc6eff7744d7
Runjing <runjing_liu@berkeley.edu>
2017-06-23 15:37:36 -0700

    univariate normal array

--- VariationalBayes/DirichletParams.py
+++ VariationalBayes/DirichletParams.py
@@ -61,3 +61,58 @@ class DirichletParamVector(object):
         return self.__vector_size
     def dim(self):
         return self.__dim
+
+# each row is a draw from a Dirichlet distribution
+class DirichletParamArray(object):
+    def __init__(self, name='', shape=(2,2), min_alpha = 0.0):
+        self.name = name
+        self.__shape = shape
+        assert min_alpha >= 0, 'alpha parameter must be non-negative'
+        self.alpha = par.ArrayParam(name + '_alpha', shape=dim, lb=min_alpha)
+        self.__free_size = self.alpha.free_size()
+        self.__vector_size = self.alpha.vector_size()
+
+    def __str__(self):
+        return self.name + ':\n' + str(self.alpha)
+    def names(self):
+        return self.alpha.names()
+    def dictval(self):
+        return { 'alpha': self.alpha.dictval() }
+
+    def e(self):
+        return self.alpha.get() / np.sum(self.alpha.get(), 1)
+
+    def e_log(self):
+        digamma_sum = asp.special.digamma(np.sum(self.alpha.get()))
+        return np.log(self.alpha.get()) / digamma_sum
+
+    def set_free(self, free_val):
+        self.alpha.set_free(free_val)
+
+    def get_free(self):
+        return self.alpha.get_free()
+
+    def free_to_vector(self, free_val):
+        self.set_free(free_val)
+        return self.get_vector()
+
+    def free_to_vector_jac(self, free_val):
+        return self.alpha.free_to_vector_jac(free_val)
+
+    def free_to_vector_hess(self, free_val):
+        return self.alpha.free_to_vector_hess(free_val)
+
+    def set_vector(self, vec):
+        if vec.size != self.__vector_size:
+            raise ValueError("Wrong size.")
+        self.alpha.set(vec)
+
+    def get_vector(self):
+        return self.alpha.get_vector()
+
+    def free_size(self):
+        return self.__free_size
+    def vector_size(self):
+        return self.__vector_size
+    def dim(self):
+        return self.__dim
--- VariationalBayes/NormalParams.py
+++ VariationalBayes/NormalParams.py
@@ -197,3 +197,72 @@ class UVNParamVector(object):
         return self.__free_size
     def vector_size(self):
         return self.__vector_size
+
+# Array of univariate normals
+# I should just do this with an array of multivariate normals...
+# unsure at the moment how to handle a collection of matrices though
+class UVNArray(object):
+    def __init__(self, name='', shape=[2,2], min_info=0.0):
+        self.name = name
+        self.__shape = shape
+        self.mean = par.ArrayParam(name + '_mean', shape)
+        self.info = par.ArrayParam(name + '_info', shape, lb=min_info)
+        self.__free_size = self.mean.free_size() + self.info.free_size()
+        self.__vector_size = self.mean.vector_size() + self.info.vector_size()
+    def __str__(self):
+        return self.name + ':\n' + str(self.mean) + '\n' + str(self.info)
+    def names(self):
+        return self.mean.names() + self.info.names()
+    def dictval(self):
+        return { 'mean': self.mean.dictval(), 'info': self.info.dictval() }
+    def e(self):
+        return self.mean.get()
+    def e2(self):
+        return self.mean.get()**2 + 1 / self.mean.info.get()
+
+    def set_free(self, free_val):
+        if free_val.size != self.__free_size: \
+            raise ValueError('Wrong size for UVNArray ' + self.name)
+        offset = 0
+        offset = par.set_free_offset(self.mean, free_val, offset)
+        offset = par.set_free_offset(self.info, free_val, offset)
+    def get_free(self):
+        return np.hstack([ self.mean.get_free(), self.info.get_free() ])
+
+    def free_to_vector(self, free_val):
+        self.set_free(free_val)
+        return self.get_vector()
+
+    # concerned about the two classes below atm since we now have matrices  ...
+    def free_to_vector_jac(self, free_val):
+        free_offset = 0
+        vec_offset = 0
+        free_offset, vec_offset, mean_jac = free_to_vector_jac_offset(
+            self.mean, free_val, free_offset, vec_offset)
+        free_offset, vec_offset, info_jac = free_to_vector_jac_offset(
+            self.info, free_val, free_offset, vec_offset)
+        return block_diag((mean_jac, info_jac))
+    def free_to_vector_hess(self, free_val):
+        free_offset = 0
+        full_shape = (self.free_size(), self.free_size())
+        hessians = []
+        free_offset = free_to_vector_hess_offset(
+            self.mean, free_val, hessians, free_offset, full_shape)
+        free_offset = free_to_vector_hess_offset(
+            self.info, free_val, hessians, free_offset, full_shape)
+        return np.array(hessians)
+
+    def set_vector(self, vec):
+        if vec.size != self.__vector_size: raise ValueError("Wrong size.")
+        offset = 0
+        offset = par.set_vector_offset(self.mean, vec, offset)
+        offset = par.set_vector_offset(self.info, vec, offset)
+    def get_vector(self):
+        return np.hstack([ self.mean.get_vector(), self.info.get_vector() ])
+
+    def free_size(self):
+        return self.__free_size
+    def vector_size(self):
+        return self.__vector_size
+    def dim(self):
+        return self.__dim
