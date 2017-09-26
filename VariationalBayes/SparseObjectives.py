897ef2c375bfec7e6c9b93413621a310399daaa2
Ryan Giordano <rgiordan@gmail.com>
2017-09-26 14:08:53 -0700

    Preconditioning tests

--- VariationalBayes/SparseObjectives.py
+++ VariationalBayes/SparseObjectives.py
@@ -71,6 +71,7 @@ class Objective(object):
         self.fun_vector_hessian = autograd.hessian(self.fun_vector)
         self.fun_vector_hvp = autograd.hessian_vector_product(self.fun_vector)
 
+        self.preconditioner = None
         self.logger = Logger()
 
     def fun_free(self, free_val, verbose=False):
@@ -84,6 +85,32 @@ class Objective(object):
         self.par.set_vector(vec_val)
         return self.fun()
 
+    # Pre-conditioned versions of the free functions.  The value at which
+    # they are evaluted is assumed to include the preconditioner, i.e.
+    # to be free_val = a * x
+    def fun_free_cond(self, free_val):
+        assert self.preconditioner is not None
+        return self.fun_free(np.matmul(self.preconditioner, free_val))
+
+    def fun_free_grad_cond(self, free_val):
+        assert self.preconditioner is not None
+        grad = self.fun_free_grad(free_val)
+        return np.matmul(self.preconditioner.T, grad)
+
+    def fun_free_hessian_cond(self, free_val):
+        assert self.preconditioner is not None
+        hess = self.fun_free_hessian(free_val)
+        return np.matmul(np.matmul(self.preconditioner.T, hess),
+                         self.preconditioner)
+
+    def fun_free_hvp_cond(self, free_val, vec):
+        assert self.preconditioner is not None
+        return np.matmul(
+            self.preconditioner.T,
+            self.fun_free_hvp(free_val,
+                              np.matmul(self.preconditioner, vec)))
+
+
 # Note: the function get_sparse_hessian, with the associated template in
 # LogisticGLMM, probably makes this class unnecessary.
 #
@@ -93,153 +120,153 @@ class Objective(object):
 # and respectively return gradients and hessians of fun() with respect
 # to the vector representations of the local parameters.  This is most useful
 # when the local grad and / or hessian are sparse.
-class SparseObjective(object):
-    def __init__(self, par, fun,
-                 global_par_name='global',
-                 local_par_name='local',
-                 fun_vector_local_grad=None,
-                 fun_vector_local_hessian=None):
-
-        self.par = par
-        self.global_par = self.par[global_par_name]
-        self.local_par = self.par[local_par_name]
-        self.fun = fun
-
-        # par should contain only the global and local params, and in
-        # that order.
-        assert len(self.par.param_dict.items()) == 2
-        par_keys = list(self.par.param_dict.keys())
-        assert par_keys[0] == global_par_name
-        assert par_keys[1] == local_par_name
-
-        # Use the dense autograd defaults if the local gradients and
-        # Hessian are not specified.
-        if not fun_vector_local_grad:
-            self.fun_vector_local_grad = \
-                autograd.grad(self.fun_vector_split, argnum=1)
-        else:
-            self.fun_vector_local_grad = fun_vector_local_grad
-
-        if not fun_vector_local_hessian:
-            self.fun_vector_local_hessian = \
-                autograd.hessian(self.fun_vector_split, argnum=1)
-        else:
-            self.fun_vector_local_hessian = fun_vector_local_hessian
-
-        # Use autograd to define the global derivatives.
-        self.fun_free_global_grad = \
-            autograd.grad(self.fun_free_split, argnum=0)
-        self.fun_free_global_hessian = \
-            autograd.hessian(self.fun_free_split, argnum=0)
-
-        # Use autograd for the Hessian vector product.
-        self.fun_free_hvp = \
-            autograd.hessian_vector_product(self.fun_free)
-
-        self.fun_vector_global_grad = \
-            autograd.grad(self.fun_vector_split, argnum=0)
-        self.fun_vector_global_hessian = \
-            autograd.hessian(self.fun_vector_split, argnum=0)
-
-        # Calculate the vector Hessian cross term using autograd.
-        # TODO: I can't remember which direction is most efficient.
-        # I think differentiating the big vector first is best, but maybe
-        # it's the other way around.
-        # Note: I'm using the dense version here to avoid requiring the user-
-        # defined on to be differentiable by autograd.
-        # self.fun_vector_local_grad_dense = \
-        #     autograd.grad(self.fun_vector_split, argnum=1)
-        # self.fun_vector_cross_hessian = autograd.jacobian(
-        #     self.fun_vector_local_grad_dense, argnum=0)
-
-        self.fun_vector_cross_hessian = autograd.jacobian(
-            self.fun_vector_global_grad, argnum=1)
-
-    def fun_free(self, free_val, verbose=False):
-        self.par.set_free(free_val)
-        val = self.fun()
-        if verbose:
-            print('Value: ', val)
-        return val
-
-    def fun_free_split(self, global_free_val, local_free_val):
-        self.global_par.set_free(global_free_val)
-        self.local_par.set_free(local_free_val)
-        return self.fun()
-
-    def fun_vector(self, vec_val):
-        self.par.set_vector(vec_val)
-        return self.fun()
-
-    # Define the vector derivatives.
-    def fun_vector_split(self, global_vec_val, local_vec_val):
-        self.global_par.set_vector(global_vec_val)
-        self.local_par.set_vector(local_vec_val)
-        return self.fun()
-
-    def fun_vector_grad_split(self, global_vec_val, local_vec_val):
-        sp_grad = sparse.hstack([
-            self.fun_vector_global_grad(global_vec_val, local_vec_val),
-            self.fun_vector_local_grad(global_vec_val, local_vec_val) ])
-
-        # TODO: maybe sometimes you want the sparse version.
-        return np.squeeze(np.array(sp_grad.T.toarray()))
-
-    # Define the free derivatives as trasnforms of the vector derivatives.
-    def fun_vector_hessian_split(self, global_vec_val, local_vec_val):
-        self.global_par.set_vector(global_vec_val)
-        self.local_par.set_vector(local_vec_val)
-
-        global_hess = self.fun_vector_global_hessian(
-            global_vec_val, local_vec_val)
-
-        cross_hess = self.fun_vector_cross_hessian(
-            global_vec_val, local_vec_val).T
-
-        local_hess = self.fun_vector_local_hessian(
-            global_vec_val, local_vec_val)
-        sp_hess =  sp.sparse.bmat([ [global_hess,  cross_hess.T],
-                                    [cross_hess,   local_hess]], format='csr')
-
-        # TODO: maybe sometimes you want the sparse version.
-        return np.array(sp_hess.toarray())
-
-    # Use the sparse transforms to change the vector gradiend into a free
-    # gradient.
-    def fun_free_grad_sparse(self, free_val):
-        self.par.set_free(free_val)
-        global_vec_val = self.global_par.get_vector()
-        local_vec_val = self.local_par.get_vector()
-
-        fun_vector_grad = self.fun_vector_grad_split(
-            global_vec_val, local_vec_val)
-        free_to_vec_jacobian = self.par.free_to_vector_jac(free_val)
-
-        # If you don't convert to an array, it returns a matrix type, which
-        # seems to cause mysterious problems with scipy.optimize.minimize.
-        free_grad = np.squeeze(free_to_vec_jacobian.T * fun_vector_grad)
-        return free_grad
-
-    # Use the sparse transforms to change the vector Hessian into a free
-    # Hessian.
-    def fun_free_hessian_sparse(self, free_val):
-        self.par.set_free(free_val)
-        global_vec_val = self.global_par.get_vector()
-        local_vec_val = self.local_par.get_vector()
-
-        fun_vector_hessian = self.fun_vector_hessian_split(
-            global_vec_val, local_vec_val)
-
-        fun_vector_grad = self.fun_vector_grad_split(
-            global_vec_val, local_vec_val)
-
-        # This is taking most of the time.
-        fun_hessian_sparse = convert_vector_to_free_hessian(
-            self.par, free_val, fun_vector_grad, fun_vector_hessian)
-
-        # If you don't convert to an array, it returns a matrix type, which
-        # seems to cause mysterious problems with scipy.optimize.minimize.
-        return np.array(fun_hessian_sparse)
+# class SparseObjective(object):
+#     def __init__(self, par, fun,
+#                  global_par_name='global',
+#                  local_par_name='local',
+#                  fun_vector_local_grad=None,
+#                  fun_vector_local_hessian=None):
+#
+#         self.par = par
+#         self.global_par = self.par[global_par_name]
+#         self.local_par = self.par[local_par_name]
+#         self.fun = fun
+#
+#         # par should contain only the global and local params, and in
+#         # that order.
+#         assert len(self.par.param_dict.items()) == 2
+#         par_keys = list(self.par.param_dict.keys())
+#         assert par_keys[0] == global_par_name
+#         assert par_keys[1] == local_par_name
+#
+#         # Use the dense autograd defaults if the local gradients and
+#         # Hessian are not specified.
+#         if not fun_vector_local_grad:
+#             self.fun_vector_local_grad = \
+#                 autograd.grad(self.fun_vector_split, argnum=1)
+#         else:
+#             self.fun_vector_local_grad = fun_vector_local_grad
+#
+#         if not fun_vector_local_hessian:
+#             self.fun_vector_local_hessian = \
+#                 autograd.hessian(self.fun_vector_split, argnum=1)
+#         else:
+#             self.fun_vector_local_hessian = fun_vector_local_hessian
+#
+#         # Use autograd to define the global derivatives.
+#         self.fun_free_global_grad = \
+#             autograd.grad(self.fun_free_split, argnum=0)
+#         self.fun_free_global_hessian = \
+#             autograd.hessian(self.fun_free_split, argnum=0)
+#
+#         # Use autograd for the Hessian vector product.
+#         self.fun_free_hvp = \
+#             autograd.hessian_vector_product(self.fun_free)
+#
+#         self.fun_vector_global_grad = \
+#             autograd.grad(self.fun_vector_split, argnum=0)
+#         self.fun_vector_global_hessian = \
+#             autograd.hessian(self.fun_vector_split, argnum=0)
+#
+#         # Calculate the vector Hessian cross term using autograd.
+#         # TODO: I can't remember which direction is most efficient.
+#         # I think differentiating the big vector first is best, but maybe
+#         # it's the other way around.
+#         # Note: I'm using the dense version here to avoid requiring the user-
+#         # defined on to be differentiable by autograd.
+#         # self.fun_vector_local_grad_dense = \
+#         #     autograd.grad(self.fun_vector_split, argnum=1)
+#         # self.fun_vector_cross_hessian = autograd.jacobian(
+#         #     self.fun_vector_local_grad_dense, argnum=0)
+#
+#         self.fun_vector_cross_hessian = autograd.jacobian(
+#             self.fun_vector_global_grad, argnum=1)
+#
+#     def fun_free(self, free_val, verbose=False):
+#         self.par.set_free(free_val)
+#         val = self.fun()
+#         if verbose:
+#             print('Value: ', val)
+#         return val
+#
+#     def fun_free_split(self, global_free_val, local_free_val):
+#         self.global_par.set_free(global_free_val)
+#         self.local_par.set_free(local_free_val)
+#         return self.fun()
+#
+#     def fun_vector(self, vec_val):
+#         self.par.set_vector(vec_val)
+#         return self.fun()
+#
+#     # Define the vector derivatives.
+#     def fun_vector_split(self, global_vec_val, local_vec_val):
+#         self.global_par.set_vector(global_vec_val)
+#         self.local_par.set_vector(local_vec_val)
+#         return self.fun()
+#
+#     def fun_vector_grad_split(self, global_vec_val, local_vec_val):
+#         sp_grad = sparse.hstack([
+#             self.fun_vector_global_grad(global_vec_val, local_vec_val),
+#             self.fun_vector_local_grad(global_vec_val, local_vec_val) ])
+#
+#         # TODO: maybe sometimes you want the sparse version.
+#         return np.squeeze(np.array(sp_grad.T.toarray()))
+#
+#     # Define the free derivatives as trasnforms of the vector derivatives.
+#     def fun_vector_hessian_split(self, global_vec_val, local_vec_val):
+#         self.global_par.set_vector(global_vec_val)
+#         self.local_par.set_vector(local_vec_val)
+#
+#         global_hess = self.fun_vector_global_hessian(
+#             global_vec_val, local_vec_val)
+#
+#         cross_hess = self.fun_vector_cross_hessian(
+#             global_vec_val, local_vec_val).T
+#
+#         local_hess = self.fun_vector_local_hessian(
+#             global_vec_val, local_vec_val)
+#         sp_hess =  sp.sparse.bmat([ [global_hess,  cross_hess.T],
+#                                     [cross_hess,   local_hess]], format='csr')
+#
+#         # TODO: maybe sometimes you want the sparse version.
+#         return np.array(sp_hess.toarray())
+#
+#     # Use the sparse transforms to change the vector gradiend into a free
+#     # gradient.
+#     def fun_free_grad_sparse(self, free_val):
+#         self.par.set_free(free_val)
+#         global_vec_val = self.global_par.get_vector()
+#         local_vec_val = self.local_par.get_vector()
+#
+#         fun_vector_grad = self.fun_vector_grad_split(
+#             global_vec_val, local_vec_val)
+#         free_to_vec_jacobian = self.par.free_to_vector_jac(free_val)
+#
+#         # If you don't convert to an array, it returns a matrix type, which
+#         # seems to cause mysterious problems with scipy.optimize.minimize.
+#         free_grad = np.squeeze(free_to_vec_jacobian.T * fun_vector_grad)
+#         return free_grad
+#
+#     # Use the sparse transforms to change the vector Hessian into a free
+#     # Hessian.
+#     def fun_free_hessian_sparse(self, free_val):
+#         self.par.set_free(free_val)
+#         global_vec_val = self.global_par.get_vector()
+#         local_vec_val = self.local_par.get_vector()
+#
+#         fun_vector_hessian = self.fun_vector_hessian_split(
+#             global_vec_val, local_vec_val)
+#
+#         fun_vector_grad = self.fun_vector_grad_split(
+#             global_vec_val, local_vec_val)
+#
+#         # This is taking most of the time.
+#         fun_hessian_sparse = convert_vector_to_free_hessian(
+#             self.par, free_val, fun_vector_grad, fun_vector_hessian)
+#
+#         # If you don't convert to an array, it returns a matrix type, which
+#         # seems to cause mysterious problems with scipy.optimize.minimize.
+#         return np.array(fun_hessian_sparse)
 
 
 # A helper function to get sparse Hessians from models.
---
+++ VariationalBayes/test_objectives.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+
+from autograd import numpy as np
+from autograd import scipy as sp
+import numpy.testing as np_test
+import unittest
+import VariationalBayes as vb
+import VariationalBayes.SparseObjectives as obj_lib
+
+class Model(object):
+    def __init__(self, dim):
+        self.dim = dim
+        self.x = vb.VectorParam('x', size=dim, lb=-2.0, ub=5.0)
+        self.y = vb.VectorParam('y', size=dim, lb=-2.0, ub=5.0)
+        self.a_mat = np.full((dim, dim), 0.1) + np.eye(dim)
+        self.set_ones()
+
+        self.preconditioner = np.eye(dim)
+
+    def set_random(self):
+        self.x.set_free(np.random.random(self.x.free_size()))
+
+    def set_ones(self):
+        #self.x.set_vector(np.ones(self.dim))
+        self.x.set_vector(np.linspace(0., 1., self.dim))
+
+    def set_zeros(self):
+        self.x.set_vector(np.zeros(self.dim))
+
+    def f_of_x(self, x):
+        return np.matmul(x.T, np.matmul(self.a_mat, x))
+
+    def f(self):
+        return self.f_of_x(self.x.get())
+
+    def f_conditioned(self):
+        # Note that
+        # df / dy = (dx' / dy) df / dx = (dy / dx')^{-1} df / dx
+        # So the transform should multiply by the inverse of the preconditioner.
+        y_free = np.matmul(self.preconditioner, self.x.get_free())
+        self.y.set_free(y_free)
+        return self.f_of_x(self.y.get())
+
+
+
+class TestObjectiveClass(unittest.TestCase):
+    # For every parameter type, execute all the required methods.
+    def test_objective(self):
+        model = Model(dim=3)
+        objective = obj_lib.Objective(par=model.x, fun=model.f)
+
+        model.set_ones()
+        x_free = model.x.get_free()
+        x_vec = model.x.get_vector()
+
+        model.set_zeros()
+        self.assertTrue(objective.fun_free(x_free) > 0.0)
+        np_test.assert_array_almost_equal(
+            objective.fun_free(x_free), objective.fun_vector(x_vec))
+
+        grad = objective.fun_free_grad(x_free)
+        hess = objective.fun_free_hessian(x_free)
+        np_test.assert_array_almost_equal(
+            np.matmul(hess, grad), objective.fun_free_hvp(x_free, grad))
+
+        model.set_zeros()
+        self.assertTrue(objective.fun_vector(x_vec) > 0.0)
+        grad = objective.fun_vector_grad(x_vec)
+        hess = objective.fun_vector_hessian(x_vec)
+        np_test.assert_array_almost_equal(
+            np.matmul(hess, grad), objective.fun_vector_hvp(x_free, grad))
+
+        # Check the preconditioning
+        preconditioner = 2.0 * np.eye(model.dim)
+        preconditioner[model.dim - 1, 0] = 0.1 # Add asymmetry for testing!
+        objective.preconditioner = preconditioner
+
+        # Test by evaluating an objective direclty in terms of the
+        # transformed variable.
+        model.preconditioner = preconditioner
+        direct_objective = \
+            obj_lib.Objective(par=model.x, fun=model.f_conditioned)
+
+        y = np.matmul(preconditioner, x_free)
+        np_test.assert_array_almost_equal(
+            direct_objective.fun_free(x_free),
+            objective.fun_free(y))
+
+        np_test.assert_array_almost_equal(
+            objective.fun_free_cond(x_free),
+            objective.fun_free(y))
+
+        grad_cond = objective.fun_free_grad_cond(y)
+        np_test.assert_array_almost_equal(
+            direct_objective.fun_free_grad(x_free), grad_cond)
+
+        hessian_cond = objective.fun_free_hessian_cond(y)
+        np_test.assert_array_almost_equal(
+            direct_objective.fun_free_hessian(x_free),
+            hessian_cond)
+
+        np_test.assert_array_almost_equal(
+            direct_objective.fun_free_hvp(x_free, grad),
+            objective.fun_free_hvp_cond(y, grad))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
